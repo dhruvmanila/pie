89833e45fd596f6f3be46a14fcffe70226a7fb06
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-05-27 08:52:07 +0530

    refactor: function to get a list of available venvs

--- cmd/list.go
+++ cmd/list.go
@@ -16,26 +16,18 @@ var listCmd = &cobra.Command{
 	Aliases: []string{"ls"},
 	Args:    cobra.NoArgs,
 	Run: func(_ *cobra.Command, _ []string) {
-		dataDir, err := xdg.DataFile("pyvenv/")
+		venvInfo, err := getVenvInfo()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Virtualenvs location: %s\n", green.Sprint(dataDir))
-
-		entries, err := os.ReadDir(dataDir)
-		if err != nil {
-			log.Fatal(err)
-		}
+		fmt.Printf("Virtualenvs location: %s\n", green.Sprint(venvInfo.RootDir))
 
 		_, currentVenv := filepath.Split(os.Getenv("VIRTUAL_ENV"))
-		for _, entry := range entries {
-			if entry.IsDir() {
-				venvName := entry.Name()
-				if currentVenv == venvName {
-					bold.Println("* " + venvName)
-				} else {
-					bold.Println("  " + venvName)
-				}
+		for _, venvName := range venvInfo.Names {
+			if currentVenv == venvName {
+				bold.Println("* " + venvName)
+			} else {
+				bold.Println("  " + venvName)
 			}
 		}
 	},
@@ -43,4 +35,38 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+}
+
+// VenvInfo contains information regarding all the virtual environments
+// managed by the tool.
+type VenvInfo struct {
+	// RootDir is the directory where all the virtual environments are stored.
+	RootDir string
+
+	// Names is a list of all the available virtual environments.
+	Names []string
+}
+
+// getVenvInfo returns information regarding all the managed virtual environments.
+func getVenvInfo() (*VenvInfo, error) {
+	dataDir, err := xdg.DataFile("pyvenv/")
+	if err != nil {
+		return nil, err
+	}
+
+	entries, err := os.ReadDir(dataDir)
+	if err != nil {
+		return nil, err
+	}
+
+	venvNames := make([]string, 0)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			venvNames = append(venvNames, entry.Name())
+		}
+	}
+	return &VenvInfo{
+		RootDir: dataDir,
+		Names:   venvNames,
+	}, nil
 }
