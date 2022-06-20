9ca0bad3d511bf9cfb5700f76d877e6573deeaa7
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-06-20 22:34:32 +0530

    fix: make new color instead of mutating existing one

--- cmd/color.go
+++ cmd/color.go
@@ -4,7 +4,7 @@ import "github.com/fatih/color"
 
 var (
 	bold       = color.New(color.Bold)
-	yellowBold = bold.Add(color.FgYellow)
+	yellowBold = color.New(color.Bold, color.FgYellow)
 	green      = color.New(color.FgGreen)
 	red        = color.New(color.FgRed)
 	faint      = color.New(color.Faint)
