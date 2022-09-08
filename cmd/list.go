2f6674f76588f3a8d5bcf3c8951fad9ef76b024f
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-09-08 10:09:55 +0530

    chore: move fn close to its only usage

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -106,6 +107,18 @@ func getVenvs(dataDir string) ([]*VirtualEnv, error) {
 	}
 
 	return venvs, nil
+}
+
+// readProjectFile reads and returns the content of the project file
+// present in the given virtual environment directory.
+func readProjectFile(venvDir string) (string, error) {
+	projectFilePath := filepath.Join(venvDir, ".project")
+	content, err := os.ReadFile(projectFilePath)
+	if err != nil {
+		return "", err
+	}
+	content = bytes.TrimRight(content, "\n")
+	return string(content), nil
 }
 
 // getPythonVersionFromConfig returns the Python version used to create
