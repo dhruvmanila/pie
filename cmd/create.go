3ccfd0da24a7d77322126dbf2848543635fbcb9c
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-11-05 15:40:58 +0530

    fix: remove virtualenv if create cmd is cancelled

--- cmd/create.go
+++ cmd/create.go
@@ -1,17 +1,24 @@
 package cmd
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
 
 	"github.com/dhruvmanila/pyvenv/internal/project"
 	"github.com/dhruvmanila/pyvenv/internal/pythonfinder"
 	"github.com/spf13/cobra"
 )
+
+// errCommandCancelled is returned when the user cancels the command.
+var errCommandCancelled = errors.New("command cancelled")
 
 // pythonVersion is the Python version to use for creating the virtual environment.
 var pythonVersion string
@@ -37,7 +44,9 @@ The environment will be created using the builtin 'venv' module. If the
 			log.Fatal(red.Sprintf("✘ Virtualenv already exists for this project: %s", p.Name))
 		} else if errors.Is(err, fs.ErrNotExist) {
 			if err = createVenv(p); err != nil {
-				if errors.Is(err, pythonfinder.ErrVersionNotFound) {
+				if errors.Is(err, errCommandCancelled) {
+					return
+				} else if errors.Is(err, pythonfinder.ErrVersionNotFound) {
 					if pythonVersion != "" {
 						log.Fatal(red.Sprintf("✘ Python version %s does not exist!", pythonVersion))
 					} else {
@@ -81,15 +90,76 @@ func createVenv(p *project.Project) error {
 		green.Sprintf("(%s)", v.VersionInfo),
 	)
 
-	cmd := exec.Command(v.Executable, "-m", "venv", p.VenvDir, "--prompt", p.Name)
-
 	// Creating the virtual environment using the 'venv' module does not
-	// produce any output, so the only output we get is the error message
-	// if the command fails.
-	output, err := cmd.CombinedOutput()
+	// produce any output, so the only output we get is the error message,
+	// if the command fails, to stderr.
+	var stderr bytes.Buffer
+
+	cmd := exec.Command(v.Executable, "-m", "venv", p.VenvDir, "--prompt", p.Name)
+	cmd.Stderr = &stderr
+
+	if err = cmd.Start(); err != nil {
+		return commandError(err, cmd, stderr)
+	}
+
+	// stop channel is used to signal that the command has finished and
+	// to stop listening for signals.
+	stop := make(chan struct{})
+
+	// signalReceived channel will receive true if a signal was received
+	// during the command execution, and false otherwise.
+	signalReceived := make(chan bool, 1)
+	defer close(signalReceived)
+
+	// Start listening for signals.
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+		defer signal.Stop(sig)
+
+		select {
+		case <-sig:
+			// Ensure that the virtual environment is deleted if we received
+			// a signal to cancel the command.
+			os.RemoveAll(p.VenvDir)
+			signalReceived <- true
+		case <-stop:
+			signalReceived <- false
+		}
+	}()
+
+	err = cmd.Wait()
+
+	// Stop listening for signals. Now, if a signal was sent, then the
+	// goroutine has already stopped because the signalReceived channel
+	// is buffered.
+	close(stop)
+	if <-signalReceived {
+		return errCommandCancelled
+	}
+
+	// There was no signal received, so we can safely check the error.
 	if err != nil {
-		return fmt.Errorf("%s: %s", err, output)
+		return commandError(err, cmd, stderr)
 	}
 
 	return nil
+}
+
+// commandError returns a formatted error message on command failure.
+// The out parameter is the output of the command, which might contain
+// useful information about the error.
+func commandError(err error, cmd *exec.Cmd, out bytes.Buffer) error {
+	var output string
+	if out.Len() > 0 {
+		output = fmt.Sprintf("Output:\n  %s",
+			faint.Sprint(strings.ReplaceAll(out.String(), "\n", "\n  ")),
+		)
+	}
+
+	return fmt.Errorf("Command failure: %s\n  %s\n%s",
+		red.Sprint(err),
+		bold.Sprint(strings.Join(cmd.Args, " ")),
+		output,
+	)
 }
